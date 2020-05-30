0f478fb77aa32769366b97adc967702d61af01bc
Miguel Morcillo <migdress@hotmail.com>
2020-05-30 17:44:33 -0500

    refactor(repository): adjustments to routes repository

--- internal/repositories/routes.go
+++ internal/repositories/routes.go
@@ -1,7 +1,32 @@
 package repositories
 
-import "errors"
+import (
+	"errors"
+	"time"
+
+	"github.com/Globhack/ghl2020-reciapp-backend/internal/models"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
 
 var ErrRouteNotFound = errors.New("route not found")
 
 var ErrRouteAlreadyAssigned = errors.New("route already assigned")
+
+type DynamoDBRoutesRepository struct {
+	client      *dynamodb.DynamoDB
+	tableRoutes string
+}
+
+func NewDynamoDBRoutesRepository(client *dynamodb.DynamoDB, tableRoutes string) *DynamoDBRoutesRepository {
+	return &DynamoDBRoutesRepository{
+		client:      client,
+		tableRoutes: tableRoutes,
+	}
+}
+
+func (r *DynamoDBRoutesRepository) FindAvailableRoutes(
+	currentTime time.Time,
+	maxTime time.Time,
+) ([]models.Route, error) {
+	return []models.Route{}, nil
+}
