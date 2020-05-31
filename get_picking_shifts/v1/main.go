a17011c1e55ea6d63da26d93e36e4e2f8ccb909a
Miguel Morcillo <migdress@hotmail.com>
2020-05-31 04:32:55 -0500

    refactor: renamed some folders

--- get_picking_shifts/v1/main.go
+++ get_picking_shifts/v1/main.go
@@ -4,48 +4,117 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/Globhack/ghl2020-reciapp-backend/internal"
 	"github.com/Globhack/ghl2020-reciapp-backend/internal/models"
 	"github.com/Globhack/ghl2020-reciapp-backend/internal/repositories"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
-var ErrUserIDEmpty = errors.New("user_id cannot be empty")
-var ErrUserIDNotFound = errors.New("user_id not found")
+var ErrUsernameEmpty = errors.New("username cannot be empty")
 
-type Handler func(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error)
+type Handler func(ctx context.Context) (events.APIGatewayProxyResponse, error)
 
-type UserScoreRepository interface {
-	GetUserScorebyUserID(userID string) (models.User, error)
+type RoutesRepoRepository interface {
+	FindAvailableRoutes(currentTime time.Time, maxTime time.Time) ([]models.Route, error)
 }
 
-type ScoreResponse struct {
-	Username string `json:"username"`
-	Score    int    `json:"score"`
+type TimeHelper interface {
+	NowWithTimezone() (time.Time, error)
+	ToISO8601(d time.Time) (string, error)
 }
 
-func Adapter(userRepo UserScoreRepository) Handler {
-	return func(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+type ResponseRoutePickingPoint struct {
+	ID         string  `json:"id"`
+	Name       string  `json:"name"`
+	LocationID string  `json:"locationid"`
+	Country    string  `json:"country"`
+	City       string  `json:"city"`
+	Latitude   float64 `json:"latitude"`
+	Longitude  float64 `json:"longitude"`
+	Address1   string  `json:"address1"`
+	Address2   string  `json:"address2"`
+}
 
-		usertID := req.PathParameters["user_id"]
+type ResponseRoute struct {
+	ID            string   `json:"id"`
+	Materials     []string `json:"materials"`
+	Sector        string   `json:"sector"`
+	Shift         string   `json:"shift"`
+	Date          string   `json:"date"`
+	PickingPoints []ResponseRoutePickingPoint
+}
 
-		if usertID == "" {
-			return internal.Error(http.StatusBadRequest, ErrUserIDEmpty), nil
+type Response struct {
+	Routes []ResponseRoute `json:"routes"`
+}
+
+func Adapter(
+	routesRepo RoutesRepoRepository,
+	hoursOffset int,
+	timeHelper TimeHelper,
+) Handler {
+	return func(ctx context.Context) (events.APIGatewayProxyResponse, error) {
+
+		// Calculate window time to query for routes
+		now, err := timeHelper.NowWithTimezone()
+		if err != nil {
+			return internal.Error(http.StatusInternalServerError, err), nil
 		}
+		maxTime := now.Add(time.Hour * time.Duration(hoursOffset))
 
-		user, err := userRepo.GetUserScorebyUserID(usertID)
+		// Query for routes
+		log.Printf("finding routes between (%v) and (%v)\n", now, maxTime)
+		routes, err := routesRepo.FindAvailableRoutes(now, maxTime)
 		if err != nil {
 			if err == repositories.ErrUserNotFound {
 				return internal.Error(http.StatusNotFound, err), nil
 			}
+
 			return internal.Error(http.StatusInternalServerError, err), nil
 		}
+		log.Printf("got %v routes\n %#v", len(routes), routes)
 
-		response := ScoreResponse{
-			Username: user.Username,
-			Score:    user.Score,
+		// Prepare response
+		responseRoutes := make([]ResponseRoute, len(routes))
+		for i, route := range routes {
+			responseRoutesPickingPoints := make([]ResponseRoutePickingPoint, len(route.PickingPoints))
+			for j, pp := range route.PickingPoints {
+				responseRoutesPickingPoints[j] = ResponseRoutePickingPoint{
+					ID:         pp.ID,
+					LocationID: pp.LocationID,
+					Country:    pp.Country,
+					City:       pp.City,
+					Latitude:   pp.Latitude,
+					Longitude:  pp.Longitude,
+					Address1:   pp.Address1,
+					Address2:   pp.Address2,
+				}
+			}
+
+			startsAt, err := timeHelper.ToISO8601(*route.StartsAt)
+			if err != nil {
+				return internal.Error(http.StatusInternalServerError, err), nil
+			}
+			responseRoutes[i] = ResponseRoute{
+				ID:            route.ID,
+				Materials:     route.Materials,
+				Sector:        route.Sector,
+				Shift:         route.Shift,
+				Date:          startsAt,
+				PickingPoints: responseRoutesPickingPoints,
+			}
+		}
+		response := Response{
+			Routes: responseRoutes,
 		}
 		jsonResponse, err := json.Marshal(response)
 		if err != nil {
@@ -57,10 +126,42 @@ func Adapter(userRepo UserScoreRepository) Handler {
 }
 
 func main() {
-	//routesTable := os.Getenv("DYNAMODB_PICKING_ROUTES")
-	// if routesTable == "" {
-	// 	panic("DYNAMODB_PICKING_ROUTES cannot be empty")
+	routesTable := os.Getenv("DYNAMODB_PICKING_ROUTES")
+	if routesTable == "" {
+		panic("DYNAMODB_PICKING_ROUTES cannot be empty")
+	}
 
-	// handler := Adapter(userRepo)
-	// lambda.Start(handler)
+	hoursOffsetString := os.Getenv("HOURS_OFFSET")
+	if hoursOffsetString == "" {
+		panic("HOURS_OFFSET cannot be empty")
+	}
+
+	hoursOffset, err := strconv.Atoi(hoursOffsetString)
+	if err != nil {
+		panic("HOURS_OFFSET must be an integer")
+	}
+
+	timezone := os.Getenv("TIMEZONE")
+	if timezone == "" {
+		panic("TIMEZONE cannot be empty")
+	}
+
+	timeHelper, err := internal.NewTimeHelper(timezone)
+	if err != nil {
+		panic(err)
+	}
+
+	uuidHelper := internal.NewUUIDHelper()
+
+	session := session.New()
+	dynamodbClient := dynamodb.New(session)
+
+	routesRepo := repositories.NewDynamoDBRoutesRepository(
+		dynamodbClient,
+		routesTable,
+		timeHelper,
+		uuidHelper,
+	)
+	handler := Adapter(routesRepo, hoursOffset, timeHelper)
+	lambda.Start(handler)
 }
